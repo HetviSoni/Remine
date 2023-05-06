587e41f0b3c0f28baac916864ea4066b47fff858
HetviSoni <202051178@iiitvadodara.ac.in>
2023-05-06 17:44:27 +0530

    home-page made

---
+++ src/components/homepage/Home.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import Navbar from '../navbar/Navbar';
+import Services from '../services/Services';
+import About from '../about/About';
+import Footer from '../footer/Footer';
+import Landing from '../landing/Landing';
+import './home.css';
+
+const Homepage = () => {
+
+  return (
+    <div className="homepage">
+      <Navbar />
+      <Landing />
+      <About />
+      <Services />
+      <Footer />
+    </div>
+  );
+};
+
+export default Homepage;
